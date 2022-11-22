b3e552bae29915c7ed4f326b44d8d70e5428db58
soyoung <thgml1444@naver.com>
2022-11-22 17:05:51 +0900

    17_로또 순위 문제풀이

---
+++ 항해2주차_달리기반/17_로또순위/17_로또순위.js
@@ -0,0 +1,13 @@
+let lottos = [44, 1, 0, 0, 31, 25]
+let win_nums = [31, 10, 45, 1, 6, 19]	
+
+function solution(lottos, win_nums) {
+  var answer = [];
+  const min = lottos.filter((item,idx)=>{
+    
+  })
+  return answer;
+}
+
+solution(lottos, win_nums)
+
---
+++ 항해2주차_달리기반/17_로또순위/README.md
@@ -0,0 +1,19 @@
+## 문제
+**내용**<br>
+- 
+
+## 계획(생각의흐름...ㅋ) 
+-  <br>
+>
+
+
+
+<br>
+
+## Think
+-
+
+<br>
+
+## url
+https://school.programmers.co.kr/learn/courses/30/lessons/77484
