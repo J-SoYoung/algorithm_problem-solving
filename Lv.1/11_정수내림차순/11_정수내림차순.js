b061c6fb5872540ed7ff6e65739fa302b062821b
soyoung <thgml1444@naver.com>
2022-11-24 01:28:17 +0900

    13_정수내림차순 문제풀이

--- Lv.1/11_정수내림차순/11_정수내림차순.js
+++ Lv.1/11_정수내림차순/11_정수내림차순.js
@@ -16,8 +16,3 @@ function solution(n) {
     
 solution(n)
 
-
-// 배열 메소드의 종류에 대해 더 공부하면 좋겠다
-// Q. let arr = [] 초기값을 넣어주는 것과
-// let arr = new Array() 로 배열을 만드는 것의 차이점은? 
-// 정리 : https://www.notion.so/newArray-f97076971ce247bfb214b976386c744f
