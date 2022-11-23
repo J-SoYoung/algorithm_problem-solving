38d1e52097e1929f339845530a73049d17554bf7
soyoung <thgml1444@naver.com>
2022-11-24 01:28:00 +0900

    12_나머지가 1이 되는 문제풀이

--- Lv.1/12_나머지가1되는/12_나머지가1되는.js
+++ Lv.1/12_나머지가1되는/12_나머지가1되는.js
@@ -1,2 +1,29 @@
+let n = 12
+// let num = 4
 
-// n나누기 
+// 1_풀이 내생각
+function solution(n) {
+  var answer = 0;
+  let arr = []
+
+  for (let i = 0; i < n; i++){
+    if (n % i === 1) arr.push(i) 
+  }
+  // for문의 결과값을 굳이 arr에 넣지 않아도 
+  // 제일 처음 true가 나온 i가 최소숫자이므로
+  // return i로 끝날 수 있다. 
+  return answer = Math.min(...arr)
+}
+solution(n)
+
+
+// 2_코드 줄이기. 불필요한 코드는 생략
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 0; i < n; i++){
+    if (n % i === 1) return answer = i
+  }
+}
+solution(n)
+
