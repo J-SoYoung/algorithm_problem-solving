670e68ff59b715061d6d146d1fc7867eaf98dc41
soyoung <thgml1444@naver.com>
2022-11-24 09:54:43 +0900

    16_핸드폰 번호 가리기 문제풀이

---
+++ Lv.1/16_핸드폰번호_가리기/16_핸드폰번호_가리기.js
@@ -0,0 +1,16 @@
+// let phone_number = "01033334444"	
+let phone_number = "027778888"	
+
+
+function solution(phone_number) {
+  var answer = '';
+  let star = '*'.repeat(phone_number.length-4)
+  let num = phone_number.slice(-4)
+
+  // console.log(star + num)
+  answer = star + num
+
+  return answer;
+}
+solution(phone_number)
+
