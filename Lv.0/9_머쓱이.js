7d9367d36db2a3aea177bf4cbe1f78620571dedd
soyoung <thgml1444@naver.com>
2022-12-08 09:24:50 +0900

    프로그래머스 Lv_0 머쓱이 - 문제 내용을 즉각 파악하는 연습을 하자

---
+++ Lv.0/9_머쓱이.js
@@ -0,0 +1,18 @@
+// 프로그래머스 Lv.0 머쓱이보다 키 큰 사람
+// https://school.programmers.co.kr/learn/courses/30/lessons/120585
+
+// 문제요약
+// 머쓱이 height보다 키가 큰 친구들을 array에서 찾아서 몇 명인지 출력해라
+
+let array = [149, 180, 192, 170]
+let height = 167
+
+
+function solution(array, height) {
+  var answer = 0;
+  answer = array.filter((ele)=>ele > height)
+  console.log(answer.length)
+  return answer.length;
+}
+
+solution(array, height)
