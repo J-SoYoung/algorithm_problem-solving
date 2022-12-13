d27e5c323987cbbca088fca8129e43de9e51e1e8
soyoung <thgml1444@naver.com>
2022-12-13 09:14:48 +0900

    프로그래머스 Lv_0 배열 2배 만들기

---
+++ Lv.0/10_배열두배만들기.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+// 원본배열 *2를 만들어라
+
+
+// let numbers = [1, 2, 3, 4, 5]
+let numbers = [1, 2, 100, -99, 1, 2, 3]
+// let num = 4
+// let num = 4
+
+
+function solution(numbers) {
+  var answer = [];
+  answer = numbers.map((n)=> n*2)
+  console.log(answer)
+  return answer;
+}
+solution(numbers)
+
