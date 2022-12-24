961a488e24bf2c668e246881242d1b9baac99cdb
thdud <thgml1444@naver.com>
2022-12-24 09:49:46 +0900

    update_filename

---
+++ Lv.0/13_아이스아메.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120819
+// 프로그래머스 Lv0 - 아이스아메리카노
+// 프로그래머스 Lv-0 아이스아메, 정수로 떨어졌을 때 말고도 이외의 값을 생각하자
+
+let money = 15000
+
+function solution(money) {
+  var answer = [];
+  answer = [Math.floor( money / 5500 )]
+  const result = (money-(5500*(answer)))
+  answer.push(result)
+  console.log(answer)
+  
+  return answer;
+}
+
+solution(money)
+
