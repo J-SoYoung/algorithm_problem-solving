373909848fb6da132d583ae8e087efa168454879
thdud <thgml1444@naver.com>
2022-12-27 09:07:55 +0900

    프로그래머스  Lv0_피자나눠먹기2

---
+++ Lv.0/14_피자나눠먹기2.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120814
+
+let n = 1
+// let n = 4
+
+
+function solution(n) {
+  var answer = 0;
+  answer = math.ceil(n/7)
+  return answer;
+}
+
+
+solution(n)
