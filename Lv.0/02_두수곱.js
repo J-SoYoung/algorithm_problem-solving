850406e912ea9aa2b27f720c9905eed8d0002224
thdud <thgml1444@naver.com>
2023-01-23 09:31:37 +0900

    두 숫자를 곱하라

---
+++ Lv.0/02_두수곱.js
@@ -0,0 +1,13 @@
+// 두 수를 곱하라
+// https://school.programmers.co.kr/learn/courses/30/lessons/120804
+
+let num1 = 4;
+let num2 = 3;
+
+function solution(num1, num2) {
+  var answer = 0;
+  answer = num1 * num2;
+  return answer;
+}
+
+solution(num1, num2);
