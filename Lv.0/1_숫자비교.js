a15b4e091f4d854d97cdef270bbcc32eefa3f1d0
thdud <thgml1444@naver.com>
2023-01-23 09:23:07 +0900

    삼항연산자를 활용하여 두 숫자를 비교하였다

---
+++ Lv.0/1_숫자비교.js
@@ -0,0 +1,13 @@
+let num1 = 4
+let num2 = 4
+// let num = 4
+
+
+function solution(num1, num2) {
+  var answer = 0;
+  num1 === num2 ? answer = 1 : answer = -1
+  console.log(answer)
+  return answer;
+}
+
+solution(num1, num2)
