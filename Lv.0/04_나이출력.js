d0bcfb099317766a5bb77fe69ce7560856b9b8f0
thdud <thgml1444@naver.com>
2023-01-23 09:33:34 +0900

    2022를 기준으로 나이를 출력하라

--- Lv.0/04_나이출력.js
+++ Lv.0/04_나이출력.js
@@ -1,11 +1,9 @@
+// 2022년도를 기준으로 나이를 출력하라
+// https://school.programmers.co.kr/learn/courses/30/lessons/120820
 let age = 40
-// let num = 4
-
-
 function solution(age) {
   var answer = 0;
   answer = 2022 - (age - 1)
-  console.log(answer)
   return answer;
 }
 
