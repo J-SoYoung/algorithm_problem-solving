4d1c5162fb583f698dc42e876363f45bc47a7f34
thdud <thgml1444@naver.com>
2023-01-23 09:48:16 +0900

    나누기를 한 후에는 정수/소수의 결과값이 나올 수 있음을 인지하라. Math.floor를 활용하여 나머지는 내림하였다

--- Lv.0/13_아이스아메.js
+++ Lv.0/13_아이스아메.js
@@ -1,5 +1,4 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120819
-// 프로그래머스 Lv0 - 아이스아메리카노
 // 프로그래머스 Lv-0 아이스아메, 정수로 떨어졌을 때 말고도 이외의 값을 생각하자
 
 let money = 15000
@@ -9,7 +8,6 @@ function solution(money) {
   answer = [Math.floor( money / 5500 )]
   const result = (money-(5500*(answer)))
   answer.push(result)
-  console.log(answer)
   
   return answer;
 }
