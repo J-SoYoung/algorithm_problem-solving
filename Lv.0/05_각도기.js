5632156aad8b6641f91507c7218c322030e86ef5
thdud <thgml1444@naver.com>
2023-01-23 09:35:29 +0900

    if 조건문을 활용하여 예각,직각,둔각을 판별하였다. 할당연산자의 의미를 헷갈려했음

--- Lv.0/05_각도기.js
+++ Lv.0/05_각도기.js
@@ -1,3 +1,6 @@
+// 예각, 둔각, 직각을 각각 구하라
+// https://school.programmers.co.kr/learn/courses/30/lessons/120829
+
 // let angle = 70
 // let angle = 90
 let angle =180
@@ -10,9 +13,6 @@ function solution(angle) {
   else if (angle == 90) answer ='직각'
   else if ( angle > 90 && angle < 180) answer ='둔각'
   else if ( angle == 180) answer ='평각'
-
-
-  console.log(answer)
 }
 
 solution(angle)
@@ -21,4 +21,3 @@ solution(angle)
 // 너무 충격적이었다. 틀린 부분이 없는데 왜 자꾸 안나오나 했더니
 // ' = ' 하나는 값을 할당하는 연산자 였다.
 // 당연한 걸, 너무 당연해서 아무렇지 않게 쓰고 있었다.
-// 앗!!!!!!!!!!!!!!!!!!!!!!!!!!!
