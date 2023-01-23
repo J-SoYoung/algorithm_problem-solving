2ee98a69c9e534611f53698b267dd3acd8ff4e28
thdud <thgml1444@naver.com>
2023-01-23 09:36:28 +0900

    두 수를 나눠라

--- Lv.0/06_두수의나눗셈.js
+++ Lv.0/06_두수의나눗셈.js
@@ -1,3 +1,6 @@
+// 두 수를 나눠라
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
 // let num1 = 3
 // let num2 = 2
 // let num1 = 7
@@ -9,7 +12,6 @@ let num2 = 16
 function solution(num1, num2) {
   var answer = ''
   answer = Math.floor(( num1 / num2 ) * 1000)
-  console.log(answer)
   return answer;
 }
 
