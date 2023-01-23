6ea75fc564c21326aa9b60f4b727ae93f876158e
thdud <thgml1444@naver.com>
2023-01-23 09:46:26 +0900

    Math.ceil()메서드를 활용하여 나눗셈 한 후, 나머지까지 계산하지 않고 올림을 하였다.

--- Lv.0/11_피자나눠먹기.js
+++ Lv.0/11_피자나눠먹기.js
@@ -1,7 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120816
 // 프로그래머스 Lv_0 피자 나눠먹기
 
-
 // let slice = 7
 // let n = 10
 let slice = 4
@@ -10,13 +9,11 @@ let n = 12
 
 function solution(slice, n) {
   var answer = 0;
-  console.log(Math.ceil(n/slice))
+  answer = Math.ceil(n/slice)
   return answer;
 }
 
-
 solution(slice, n)
-
 
 // think
 // 사람수 나누기 피자조각 => 딱 떨어지지 안흐면 올림
