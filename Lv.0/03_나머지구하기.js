210ac83d17110d85d9e6592bb5fbde1219e1a2f0
thdud <thgml1444@naver.com>
2023-01-23 09:32:24 +0900

    나머지 연산자를 활용하여 나머지를 구하라

--- Lv.0/03_나머지구하기.js
+++ Lv.0/03_나머지구하기.js
@@ -1,10 +1,12 @@
+// 나머지를 구하라
+// https://school.programmers.co.kr/learn/courses/30/lessons/120810
+
 let num1 = 10
 let num2 = 5
 
 function solution(num1, num2) {
   var answer = 0;
   answer = num1 % num2
-  console.log(answer) 
   return answer;
 }
 
