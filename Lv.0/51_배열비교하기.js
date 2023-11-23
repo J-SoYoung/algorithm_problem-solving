90f06998bfd9cf7718f47a41b61292312b1dbd10
soyoung <thgml1444@naver.com>
2023-11-24 07:05:17 +0900

    reduce로 배열 원소의 누적합계를 구하는 기능을 함수로 빼서 구현하였다

--- Lv.0/51_배열비교하기.js
+++ Lv.0/51_배열비교하기.js
@@ -1,23 +1,30 @@
-const arr1 = [49, 13]	
-const arr2 = [70, 11, 2]	
+// 배열 비교하기 
+// https://school.programmers.co.kr/learn/courses/30/lessons/181856
 
-// const arr1 = [100, 17, 84, 1]
-// const arr2 = [55, 12, 65, 36]
+// const arr1 = [49, 13]	
+// const arr2 = [70, 11, 2]	
+
+const arr1 = [100, 17, 84, 1]
+const arr2 = [55, 12, 65, 36]
 
 // const arr1 =[1, 2, 3, 4, 5]
 // const arr2 =[3, 3, 3, 3, 3]
 
+function reduceFunc(arr){
+  const result = arr.reduce((acc, cur) => acc + cur )
+  return result
+}
 
 function solution(arr1, arr2) {
   var answer = 0;
   if (arr1.length != arr2.length){
   arr1.length > arr2.length ? answer = 1 : answer = -1
-}else { 
-  const sum_arr1 = arr1.reduce((acc, cur) => acc + cur )
-  const sum_arr2 = arr2.reduce((acc, cur) => acc + cur )
+}else {
+  const sum_arr1 = reduceFunc(arr1)
+  const sum_arr2 = reduceFunc(arr2)
   sum_arr1 == sum_arr2 ? answer = 0 : sum_arr1 > sum_arr2 ? answer = 1 : answer = -1 
 }
-  return answer;
+return answer;
 }
 
 
