87a6625b689002b25d4260555d1827d5478c7d93
thdud <thgml1444@naver.com>
2024-10-23 11:42:26 +0900

    업뎃

--- 항해99/도형시험.js
+++ 항해99/도형시험.js
@@ -1,6 +1,5 @@
-
-// let star= 6;
-// let s = ''
+let star = 5;
+let s = "";
 
 // function solution(star){
 
@@ -13,34 +12,28 @@
 //     for (let b=0; b<(2*i-1); b++){
 //       s += '*'
 //     }
-//     s+= '\n'  
+//     s+= '\n'
 //   }
 //   console.log(s)
 // }
 // solution(star)
 
-
-let star= 6;
-
-function solution(star){
-
-  let s = '';
+function solution(star) {
   // 줄 구하기
-  for (let i=0; i<=star; i++){
-    console.log(`${i}번`)
-    // 공백넣기 
-    for (let a=star; a>i; a --){
-      // console.log(i)
-      s += '-'
-      console.log(s)
+  for (let i = 0; i < star; i++) {
+    console.log(`${i}번`);
+
+    // 공백넣기
+    for (let a = star; a > i; a--) {
+      s += "-";
     }
     // 별 넣기
-    for (let b=0; b<(2*i-1); b++){
-      s += '*'
+    for (let b = 0; b < 2 * i - 1; b++) {
+      s += "*";
+      console.log(s);
     }
     //쭉 이어지니까 줄바꿈 넣기
-    s+= '\n'  
+    s += "\n";
   }
-  // console.log(s)
 }
-solution(star)
+solution(star);
