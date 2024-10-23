6f5898c64b80a36d479ca4841e77bb8894108537
thdud <thgml1444@naver.com>
2024-10-23 12:29:28 +0900

    자료구조 배열 정리

--- Book-CodingTest/01_array/01_배열정렬하기.js
+++ Book-CodingTest/01_array/01_배열정렬하기.js
@@ -14,13 +14,22 @@ function solution(num) {
 
 solution(num);
 
-function bubbleSort(arr){
-  const n = arr.length
-  for(let i=0; i<n; i++){
-    for (let j=0; j <n-1; j++){
-      if(arr[j+1] < arr[j]){
-        
+// 시간복잡도 O(N^2)
+function bubbleSort(arr) {
+  const n = arr.length;  
+  for (let i = 0; i < n; i++) {  
+    for (let j = 0; j < n - i - 1; j++) { 
+      if (arr[j + 1] < arr[j]) {  
+        // 두 요소를 비교하여 교환
+        const tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
       }
     }
   }
+  return arr;  // 정렬된 배열 반환
 }
+
+// 테스트: [1, -5, 2, 4, 3] 정렬
+const arr = [1, -5, 2, 4, 3];
+console.log(bubbleSort(arr));
