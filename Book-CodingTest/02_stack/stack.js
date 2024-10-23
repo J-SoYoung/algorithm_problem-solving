c56319332c26b72656c52e6353ba6f6c8621ea66
thdud <thgml1444@naver.com>
2024-10-23 12:29:57 +0900

    자료구조 stack정리

---
+++ Book-CodingTest/02_stack/stack.js
@@ -0,0 +1,36 @@
+/** 스택에 필요한 구현
+ * stack = []
+ * 스택에 데이터를 넣고 push 빼는 pop 함수가 있어야하고
+ * 스택이 꽉 찼는지 비어있는지 확인 isFull isEmpty
+ */
+
+const stack = [];
+const maxSize = 10;
+
+function isFull(stack) {
+  return stack.length === maxSize;
+}
+
+function isEmpty(stack) {
+  return stack.length === 0;
+}
+
+function push(stack, item) {
+  if(stack.length >= maxSize){
+    console.log('스택이 가득 찼습니다. 데이터 추가를 못했습니다')
+    return stack
+  }else {
+    console.log('스택에 추가되었습니다')
+    return stack.push(item);
+  }
+}
+
+function pop(stack, item) {
+  if(stack.isEmpty(stack)){
+    console.log('스택이 비어있습니다')
+    return 
+  }else {
+    console.log('item을 제거했습니다')
+    return stack.pop(item);
+  }
+}
