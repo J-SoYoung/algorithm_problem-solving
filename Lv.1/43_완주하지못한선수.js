e75f7af8f5556d410b5e3d41f7b5cc2a7dc5330f
thdud <thgml1444@naver.com>
2024-11-21 12:07:05 +0900

    문제작성

--- Lv.1/43_완주하지못한선수.js
+++ Lv.1/43_완주하지못한선수.js
@@ -1,3 +1,12 @@
+/** 완주하지 못한 선수
+ * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 
+ * 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+ * 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+ * 
+ * "leo"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+ * "mislav"는 참여자 명단에 두 명, 완주자 명단에는 한 명, 한명은 완주하지 못했습니다.
+ */
+
 function solution(participant, completion) {
   let goal = new Map();
 
